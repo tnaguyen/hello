149ca197b7ffc019545bd86ac1210678e0d6db1e
Tai Nguyen <tnguyen161@huskers.unl.edu>
2021-08-27 16:34:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tai Nguyen
+ *         tnguyen161@huskers.unl.edu
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tai Nguyen\n");
+
+  return 0;
